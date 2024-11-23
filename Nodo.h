ad0e6176672f854ae6b31164b8a535f9c3b834cb
soufiane7070 <140196518+soufiane7070@users.noreply.github.com>
2024-11-23 13:03:11 +0100

    1st_ed_lib
    funzione inserisci,
    attributo weight

--- Nodo.h
+++ Nodo.h
@@ -9,50 +9,18 @@
 #include <algorithm>
 #include <sstream>
 using namespace std;
+/// modifiche in corso della classe: funzioni gia modificate: inserisci;
 class nodo{
     int val;
 	nodo* fd;
 	nodo* fs;
-
+    int weight;
     public:
 
     nodo(int val1);
   nodo(int val1,int val2);
 
-void inser(nodo*& radice, int valu) {
-	if (radice == nullptr) {
-        radice = new nodo(valu); 
-     //   cout << "si1" << endl;
-        return;
-    }
-               // cout<<"si2"<<endl;
-
-	nodo* genit;
-	nodo* corr;
-	genit = nullptr;
-	corr = radice;
-               // cout<<"si3"<<endl;
-
-	while (true) {
-		genit = corr;
-		if (valu < corr->val) {
-			corr = corr->fs;
-			if (corr == nullptr) {
-				genit->fs = new nodo(valu);
-                    //        cout<<"si4"<<endl;
-
-				return;
-			}
-		}
-		else corr = corr->fd;
-		if (corr == nullptr) {
-			genit->fd = new nodo(valu);
-           // cout<<"si5"<<endl;
-            return;
-
-		}
-	}
-}
+void inser(nodo*& radice, int valu);
 
 void print(nodo* radice) {
     if (radice == nullptr) {
--- albero.cpp
+++ albero.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 
 int main(){
+  srand(time(NULL));
   nodo radice(5);
-  nodo *punt;
-  
+  nodo *punt{&radice};
+  for(int i=0;i<10;i++){
+    int g;
+    g=rand()%23+2;
+    radice.inser(punt,g);
+  }
+  radice.print(punt);
 }
